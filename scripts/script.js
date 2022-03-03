0a3a9c0d0bf2876ac1025ee4c003be4a5fa99a80
CDman814 <81450644+CDman814@users.noreply.github.com>
2022-03-03 11:40:17 -0600

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  console.log("doc is ready");
+  // alert('yo');
+
+  // Counts and outputs number of times clicked
+    $('#buttonCount').click( function(){
+      var counter = 0;
+      counter++;
+      let message = "Clicked " + counter + " times"
+      $("#buttonOutput").html(message);
+      } );
+    } )
